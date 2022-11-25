4fceca96aacd8c9262aab5161180a648ecf3d243
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-11-25 09:23:59 +0000

    Enable account.

--- .github/workflows/test-creds.py
+++ .github/workflows/test-creds.py
@@ -1,5 +1,13 @@
+import os
 from pytket.extensions.qiskit import IBMQBackend
+from qiskit import IBMQ
 from qiskit_ibm_runtime import QiskitRuntimeService
+
+if not IBMQ.stored_account():
+    token = os.getenv("PYTKET_REMOTE_QISKIT_TOKEN")
+    if token:
+        print("Enabling account")
+        IBMQ.enable_account(token)
 
 b = IBMQBackend("ibmq_lima", hub="ibm-q", group="open", project="main")
 

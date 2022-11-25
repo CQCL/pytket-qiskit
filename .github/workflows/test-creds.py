8d97f2c509dde3d81b7992d38c9c7e8b105cf5c1
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-11-25 09:46:45 +0000

    Simplify.

--- .github/workflows/test-creds.py
+++ .github/workflows/test-creds.py
@@ -1,15 +1,17 @@
 import os
-from pytket.extensions.qiskit import IBMQBackend
+
 from qiskit import IBMQ
 from qiskit_ibm_runtime import QiskitRuntimeService
 
 if not IBMQ.stored_account():
+    print("No stored account")
     token = os.getenv("PYTKET_REMOTE_QISKIT_TOKEN")
     if token:
         print("Enabling account")
         IBMQ.enable_account(token)
 
-b = IBMQBackend("ibmq_lima", hub="ibm-q", group="open", project="main")
+provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
+b = provider.get_backend("ibmq_lima")
 
 print("Made IBMQBackend")
 

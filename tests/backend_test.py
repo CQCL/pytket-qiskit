1ae5725f0e4deeb5a583f51a1219fc5d9dcf4666
sjdilkes <silas.dilkes@cambridgequantum.com>
2023-01-23 15:35:36 +0000

    Update backend_test.py

--- tests/backend_test.py
+++ tests/backend_test.py
@@ -954,6 +954,9 @@ def test_ibmq_conditional(manila_backend: IBMQBackend) -> None:
     assert b.backend_info.supports_fast_feedforward
     compiled = b.get_compiled_circuit(c)
     assert not NoMidMeasurePredicate().verify(compiled)
+    print(compiled.get_commands())
+    for x in b.required_predicates():
+        print(x, x.verify(compiled))
     assert b.valid_circuit(compiled)
 
 

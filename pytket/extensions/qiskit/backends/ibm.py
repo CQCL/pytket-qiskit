039d7c6eb26597d798150e01edd6a2839d540237
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-06-05 15:19:51 +0100

    unsupported ops in expection handling

--- pytket/extensions/qiskit/backends/ibm.py
+++ pytket/extensions/qiskit/backends/ibm.py
@@ -297,7 +297,7 @@ class IBMQBackend(Backend):
 
     @primitive_gates.setter
     def primitive_gates(self, primitives):
-        if primitives is None:
+        if primitives != GateSet.X_SX_RZ_CX or GateSet.X_SX_RZ_ECR:
             raise UnsupportedGateSetException(primitives)
         self._primitive_gates = primitives
 

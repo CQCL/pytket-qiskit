82c1d5a25071df891946e23e321b3c272428290a
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-06-06 12:51:23 +0100

    1.15 in setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.16",
+        "pytket ~= 1.15",
         "qiskit ~= 0.42.1",
         "qiskit-ibm-runtime ~= 0.9.2",
         "qiskit-aer ~= 0.12.0",

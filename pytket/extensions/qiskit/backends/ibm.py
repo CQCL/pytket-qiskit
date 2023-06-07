a7e3133ffef9eb564748e3bdebe9292d8a7c739b
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-06-07 14:27:13 +0100

    fix name

--- pytket/extensions/qiskit/backends/ibm.py
+++ pytket/extensions/qiskit/backends/ibm.py
@@ -135,7 +135,7 @@ def _save_ibmq_auth(qiskit_config: Optional[QiskitConfig]) -> None:
             QiskitRuntimeService.save_account(channel="ibm_quantum", token=token)
 
 
-class IBMPytketBackend(Backend):
+class IBMBackend(Backend):
     _supports_shots = False
     _supports_counts = True
     _supports_contextual_optimisation = True

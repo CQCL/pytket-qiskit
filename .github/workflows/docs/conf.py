d5affc27697dbfc4cb5f273cdb6c41b07cfa5c82
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-06-30 15:03:48 +0100

    update copyright info

--- .github/workflows/docs/conf.py
+++ .github/workflows/docs/conf.py
@@ -3,7 +3,8 @@
 # Configuration file for the Sphinx documentation builder.
 # See https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-author = "Cambridge Quantum Computing Ltd"
+copyright = "2023 Quantinuum"
+author = "Quantinuum"
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -12,8 +13,6 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx_copybutton",
 ]
-
-pygments_style = "borland"
 
 html_theme = "sphinx_book_theme"
 

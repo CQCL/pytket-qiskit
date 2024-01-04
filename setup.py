3683030a85a716b3bd724c446b08ab91eae35ef7
Alec Edgington <alec.edgington@quantinuum.com>
2024-01-04 13:10:47 +0000

    Correct URL for documentation.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email="tket-support@cambridgequantum.com",
     python_requires=">=3.9",
     project_urls={
-        "Documentation": "https://tket.quantinuum.com/extensions/pytket-qiskit/api/index.html",
+        "Documentation": "https://tket.quantinuum.com/extensions/pytket-qiskit/index.html",
         "Source": "https://github.com/CQCL/pytket-qiskit",
         "Tracker": "https://github.com/CQCL/pytket-qiskit/issues",
     },

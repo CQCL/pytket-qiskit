af55f30a5fdae875dbc41ac61398636d1552ecbb
Alec Edgington <alec.edgington@quantinuum.com>
2024-02-20 13:56:44 +0000

    Update pytket version.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.24",
+        "pytket ~= 1.25",
         "qiskit ~= 0.45.0",
         "qiskit-algorithms ~= 0.2.1",
         "qiskit-ibm-runtime ~= 0.17.0",

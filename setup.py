130198cab8c14fb9f084765e93cfb478132d0257
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-04-16 08:59:44 +0100

    Update to qiskit-ibm-runtime 0.23 (#321)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "pytket ~= 1.26",
         "qiskit ~= 1.0",
         "qiskit-algorithms ~= 0.3.0",
-        "qiskit-ibm-runtime ~= 0.22.0",
+        "qiskit-ibm-runtime ~= 0.23.0",
         "qiskit-aer ~= 0.14.0",
         "qiskit-ibm-provider ~= 0.10.0",
         "numpy",

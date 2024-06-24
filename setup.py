40cb48a71cddc9e10166f6464c3a85b483085b24
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-06-24 10:02:34 +0100

    Remove upper caps on dependency versions. (#358)

--- setup.py
+++ setup.py
@@ -44,12 +44,12 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.29",
-        "qiskit ~= 1.1",
-        "qiskit-algorithms ~= 0.3.0",
-        "qiskit-ibm-runtime ~= 0.24.1",
-        "qiskit-aer ~= 0.14.2",
-        "numpy < 2",
+        "pytket >= 1.29",
+        "qiskit >= 1.1",
+        "qiskit-algorithms >= 0.3.0",
+        "qiskit-ibm-runtime >= 0.24.1",
+        "qiskit-aer >= 0.14.2",
+        "numpy >= 1.26.4",
     ],
     classifiers=[
         "Environment :: Console",

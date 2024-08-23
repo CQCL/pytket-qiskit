0378e03640d083f54e74573bd4cdf5aee8751864
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-08-23 16:24:17 +0100

    correct build docs script

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -5,3 +5,7 @@ mv pytket-docs-theming/quantinuum-sphinx .
 mv pytket-docs-theming/conf.py .
 
 sphinx-build -b html . build 
+
+mv _static pytket-docs-theming
+mv quantinuum-sphinx pytket-docs-theming 
+mv conf.py pytket-docs-theming

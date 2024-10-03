5bd5630cb3dd794ddcb0a35c03078feb2fa401cc
Melf <cqc@melf.de>
2024-10-03 18:09:47 +0100

    update pytket version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket >= 1.30.0",
+        "pytket >= 1.33.0",
         "qiskit >= 1.2.0",
         "qiskit-ibm-runtime >= 0.24.1",
         "qiskit-aer >= 0.14.2",

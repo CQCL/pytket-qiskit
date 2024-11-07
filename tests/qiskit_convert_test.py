94139f8a10c171830dfc0926c38b80e0ea64309b
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-11-07 14:01:21 +0000

    remove unused import

--- tests/qiskit_convert_test.py
+++ tests/qiskit_convert_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-import warnings
 from collections import Counter
 from math import pi
 

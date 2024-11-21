8accca14a7f36f5c44df78d69c2fe005a335afff
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-11-21 16:33:17 +0000

    fix docstring length

--- pytket/extensions/qiskit/qiskit_convert.py
+++ pytket/extensions/qiskit/qiskit_convert.py
@@ -886,8 +886,8 @@ def tk_to_qiskit(
     :param tkcirc: A :py:class:`Circuit` to be converted
     :param replace_implicit_swaps: Implement implicit permutation by adding SWAPs
         to the end of the circuit.
-    :param perm_warnings: Warn if an input circuit has implicit qubit permutations, True by
-        default.
+    :param perm_warnings: Warn if an input circuit has implicit qubit permutations,
+        True by default.
     :return: The converted circuit
     """
     tkc = tkcirc.copy()  # Make a local copy of tkcirc

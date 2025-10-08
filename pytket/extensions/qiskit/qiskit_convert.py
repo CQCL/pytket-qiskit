6145e24d274eb68ce766ddc259ec374cd1178b16
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2025-10-08 15:59:50 +0100

    fix: address the condition bits in handling of `IfElseOp` (#524)
    * upload failing test case
    
    * add fix but exclude logic for flattening conditions
    
    * fix error message
    
    * update changelog
    
    * raise an error for more that one condition bit
    
    * remove Var handling for now
    
    * give NotImplementedError for unsupported condition types
    
    * check for tuple explicitly, not via hasattr(__getitem__)
    
    * check for Expr
    
    * ruff fix
    
    * more specific error msg
    
    * changelog doc fixes

--- pytket/extensions/qiskit/qiskit_convert.py
+++ pytket/extensions/qiskit/qiskit_convert.py
@@ -82,6 +82,7 @@ from qiskit.circuit import (
     Reset,
 )
 from qiskit.circuit import Qubit as QCQubit
+from qiskit.circuit.classical.expr import Binary, Expr, Unary, Var  # type: ignore
 from qiskit.circuit.library import (
     CRYGate,
     Initialize,
@@ -675,17 +676,42 @@ def _append_if_else_circuit(
     cargs: list[Clbit],
 ) -> None:
     # Get two pytket circuits which implement the true_body and false_body.
+    # Note that else_circ can be None if no false_body is specified.
     if_circ, else_circ = _pytket_circuits_from_ifelseop(
         if_else_op, outer_builder, qargs, cargs
     )
-    # else_circ can be None if no false_body is specified.
-    if isinstance(if_else_op.condition[0], Clbit):
-        if len(bits) != 1:
-            raise NotImplementedError("Conditions on multiple bits not supported")
+
+    if isinstance(if_else_op.condition, (Unary | Binary | Var | Expr)):
+        raise NotImplementedError(
+            "Handling of Unary, Var, Binary and Expr conditions is not supported yet."
+        )
+
+    if isinstance(if_else_op.condition, tuple) and isinstance(
+        if_else_op.condition[0], Clbit
+    ):
+        condition_bits = [
+            bit
+            for bit in bits
+            if bit.reg_name == if_else_op.condition[0]._register.name  # noqa: SLF001
+            and bit.index[0] == if_else_op.condition[0]._index  # noqa: SLF001
+        ]
+
+        if len(condition_bits) == 0:
+            raise ValueError(
+                "Failed to find any pytket Bit matching Qiskit Clbit in condition for IfElseOp."
+            )
+        if len(condition_bits) > 1:
+            raise ValueError(
+                f"Found condition on {len(condition_bits)},"
+                "only conditions on single individual bits are supported at present."
+                " Check for duplicate bits or bits with multiple indices."
+            )
+
+        # In this case, if_else_op.condition is a single tuple of shape (Clbit, value)
         outer_builder.tkc.add_circbox(
             circbox=CircBox(if_circ),
             args=if_circ.qubits + if_circ.bits,  # type: ignore
-            condition_bits=bits,
+            condition_bits=condition_bits,
             condition_value=if_else_op.condition[1],
         )
         # If we have an else_circ defined, add it to the circuit
@@ -693,14 +719,16 @@ def _append_if_else_circuit(
             outer_builder.tkc.add_circbox(
                 circbox=CircBox(else_circ),
                 args=else_circ.qubits + else_circ.bits,  # type: ignore
-                condition_bits=bits,
+                condition_bits=condition_bits,
                 condition_value=1 ^ if_else_op.condition[1],
             )
-
-    elif isinstance(if_else_op.condition[0], ClassicalRegister):
+    elif isinstance(if_else_op.condition, tuple) and isinstance(
+        if_else_op.condition[0], ClassicalRegister
+    ):
         pytket_bit_reg: BitRegister = outer_builder.tkc.get_c_register(
             if_else_op.condition[0].name
         )
+
         outer_builder.tkc.add_circbox(
             circbox=CircBox(if_circ),
             args=if_circ.qubits + if_circ.bits,  # type: ignore
@@ -715,7 +743,7 @@ def _append_if_else_circuit(
     else:
         raise TypeError(
             "Unrecognized type used to construct IfElseOp. Expected "  # noqa: ISC003
-            + f"ClBit or ClassicalRegister, got {type(if_else_op.condition[0])}"
+            + f"a ClBit or ClassicalRegister tuple, got {type(if_else_op.condition)}"
         )
 
 

e9b2e6b771b9da86bd42fb05ab466f893743d929
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-13 15:43:57 +0100

    Create static method pass_from_info

--- pytket/extensions/qiskit/backends/ibm.py
+++ pytket/extensions/qiskit/backends/ibm.py
@@ -395,7 +395,13 @@ class IBMQBackend(Backend):
         timeout: int = 300,
     ) -> BasePass:
         backend_info = IBMQBackend._get_backend_info(config, props)
-        tk_gate_set = _tk_gate_set(config)
+        return IBMQBackend.pass_from_info(backend_info)
+
+    @staticmethod
+    def pass_from_info(
+        backend_info: BackendInfo, optimisation_level: int = 2, timeout: int = 300
+    ) -> BasePass:
+        tk_gate_set = backend_info.gate_set
         primitive_gates = _get_primitive_gates(tk_gate_set)
         primitive_1q_gates = _get_primitive_1q_gates(tk_gate_set)
         supports_rz = OpType.Rz in primitive_gates
